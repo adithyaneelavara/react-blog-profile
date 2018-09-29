409d29437a1105b430d99dd31c8f32268ecca2cd
Adithya Neelavara <adithya.neelavara@gmail.com>
2018-09-29 11:36:19 +0100

    Updated Get Call

--- src/blog.js
+++ src/blog.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import BlogPost from './blog-post'
+import axios from 'axios';
 const dataItems=[
 	{
 		"id":"abcd",
@@ -15,13 +16,27 @@ const dataItems=[
 	}
 ]
 class Blog extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+		posts:[]
+	};
+
+	};
 
 	  createTable() {
     	let bloglist=[];
-    	for(var i=0;i<dataItems.length;i++){
-    		bloglist.push(<div><BlogPost id={dataItems[i].id} data={dataItems[i]}/></div>)
+    	for(var i=0;i<this.state.posts.length;i++){
+    		bloglist.push(<div><BlogPost id={this.state.posts[i].id} data={this.state.posts[i]}/></div>)
      	 }
 	    return bloglist;
+  };
+    componentDidMount() {
+    axios.get(`https://op62n4xpi0.execute-api.eu-west-1.amazonaws.com/DEV/?postId=*`)
+      .then(res => {
+        const posts = res.data.map(obj => obj);
+        this.setState({ posts });
+      });
   };
 	render(){
 
